6f87c94e92364ea6f63d973290de551154271d9d
ltabis <ltabis.pro@gmail.com>
2022-08-31 23:35:35 +0200

    feat: add a non-functioning grid plugin.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
-use bevy::prelude::*;
+mod grid;
+use bevy::{prelude::*, winit::WinitSettings};
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(grid::GridPlugin {
+            grid_size: Vec2::new(10.0, 10.0),
+            cell_size: 5.0,
+            space_between_cells: 2.0,
+        })
+        // Only run the app when there is user input. This will significantly reduce CPU/GPU use.
+        .insert_resource(WinitSettings::desktop_app())
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    // Camera
+    commands.spawn_bundle(Camera2dBundle::default());
 }
