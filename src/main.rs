93c58b01b5140b5f4d43fc0e4631422ebe6ba5bc
ltabis <ltabis.pro@gmail.com>
2022-09-07 12:44:01 +0200

    chore: remove grid code.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,13 @@
-mod grid;
-use bevy::{prelude::*, winit::WinitSettings};
+use bevy::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(grid::GridPlugin {
-            grid_size: Vec2::new(10.0, 10.0),
-            cell_size: 5.0,
-            space_between_cells: 2.0,
-        })
-        // Only run the app when there is user input. This will significantly reduce CPU/GPU use.
-        .insert_resource(WinitSettings::desktop_app())
         .add_startup_system(setup)
+        .add_plugin(bevy_inspector_egui::WorldInspectorPlugin::new())
         .run();
 }
 
 fn setup(mut commands: Commands) {
-    // Camera
     commands.spawn_bundle(Camera2dBundle::default());
 }
